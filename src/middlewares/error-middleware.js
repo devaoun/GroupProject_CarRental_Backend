407c4f58e34e28d6abb01b0a87c42008b062c520
devaoun <devaoun147@gmail.com>
2024-06-22 08:52:49 +0700

    add node jsonwebtoken,bcrypt and edit error-middleware,notFound-middleware

--- src/middlewares/error-middleware.js
+++ src/middlewares/error-middleware.js
@@ -1,3 +1,10 @@
 module.exports = (error, req, res, next) => {
-    res.json({error : error.message})
+    // ถ้า error มาจาก jsonwebtoken // หรือ token หมดอายุ
+    if (err instanceof JsonWebTokenError || err instanceof TokenExpiredError) {
+        err.statusCode = 401;
+    }
+
+    res
+        .status(err.statusCode || 500)
+        .json({ message: err.message, field: err.field });
 }
