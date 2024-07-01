9afe47a09b7aec2ba6b6feadae09deef75063b5a
devaoun <devaoun147@gmail.com>
2024-07-01 14:38:02 +0700

    update backend

--- src/middlewares/rate-limit.js
+++ src/middlewares/rate-limit.js
@@ -2,7 +2,7 @@ const {rateLimit} = require('express-rate-limit');
 
 const limiter = rateLimit({
     windowMs : 1 * 60 * 1000,
-    limit: 40,
+    limit: 1000,
     message: {message: 'From limiter too many request in a given period'}
 })
 
